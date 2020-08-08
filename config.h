d66539075712d700bcb9dc9253d65db8d8cb0e05
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-08-08 14:16:15 -0400

    Modify colors to match dotfiles

--- config.h
+++ config.h
@@ -9,11 +9,11 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+  [SchemeNorm] =            { "#eceff4",        "#2e3440"       },
+	[SchemeSel] =             { "#5d62ac",  "#febdf7"  },
+	[SchemeSelHighlight] =    { "#5d62ac",  "#febdf7"  },
+	[SchemeNormHighlight] =   { "#81a1c1", "#2e3440"       },
+	[SchemeOut] =             { "#000000",          "#00ffff"         },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
--- template.config.h
+++ template.config.h
@@ -9,11 +9,11 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+  [SchemeNorm] =            { "$COLOR_FG",        "$COLOR_BG"       },
+	[SchemeSel] =             { "$COLOR_TERTIARY",  "$COLOR_PRIMARY"  },
+	[SchemeSelHighlight] =    { "$COLOR_TERTIARY",  "$COLOR_PRIMARY"  },
+	[SchemeNormHighlight] =   { "$COLOR_SECONDARY", "$COLOR_BG"       },
+	[SchemeOut] =             { "#000000",          "#00ffff"         },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
