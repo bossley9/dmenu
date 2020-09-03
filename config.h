49b557a62d33f89bcc4be6ad11d59dcea8e3222b
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-09-02 22:40:54 -0400

    Tweak colors of dmenu for better visibility

--- config.h
+++ config.h
@@ -1,23 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-  "Source Code Pro Medium:pixelsize=15:antialias=true:autohint=true"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {"Source Code Pro Medium:pixelsize=15:antialias=true:autohint=true"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-  [SchemeNorm] =            { "#eceff4",        "#2e3440"       },
-	[SchemeSel] =             { "#5d62ac",  "#febdf7"  },
-	[SchemeSelHighlight] =    { "#5d62ac",  "#febdf7"  },
-	[SchemeNormHighlight] =   { "#81a1c1", "#2e3440"       },
-	[SchemeOut] =             { "#000000",          "#00ffff"         },
+    /*     fg         bg       */
+    [SchemeNorm] = {"#eceff4", "#171520"},
+    [SchemeSel] = {"#171520", "#519aba"},
+    [SchemeSelHighlight] = {"#171520", "#519aba"},
+    [SchemeNormHighlight] = {"#519aba", "#171520"},
+    [SchemeOut] = {"#171520", "#00ffff"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-static unsigned int lineheight = 28;         /* -h option; minimum height of a menu line     */
+static unsigned int lines = 0;
+static unsigned int lineheight =
+    28; /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
@@ -26,4 +26,4 @@ static unsigned int lineheight = 28;         /* -h option; minimum height of a m
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 8;
+static const unsigned int border_width = 10;
--- template.config.h
+++ template.config.h
@@ -1,23 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-  "$FONT"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {"$FONT"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-  [SchemeNorm] =            { "$COLOR_FG",        "$C_BLACK_0"       },
-	[SchemeSel] =             { "$COLOR_TERTIARY",  "$COLOR_PRIMARY"  },
-	[SchemeSelHighlight] =    { "$COLOR_TERTIARY",  "$COLOR_PRIMARY"  },
-	[SchemeNormHighlight] =   { "$COLOR_SECONDARY", "$C_BLACK_0"       },
-	[SchemeOut] =             { "#000000",          "#00ffff"         },
+    /*     fg         bg       */
+    [SchemeNorm] = {"$C_GRAY_1", "$C_BLACK_0"},
+    [SchemeSel] = {"$C_BLACK_0", "$C_CYAN_0"},
+    [SchemeSelHighlight] = {"$C_BLACK_0", "$C_CYAN_0"},
+    [SchemeNormHighlight] = {"$COLOR_SECONDARY", "$C_BLACK_0"},
+    [SchemeOut] = {"$C_BLACK_0", "#00ffff"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-static unsigned int lineheight = $WM_BAR_HEIGHT;         /* -h option; minimum height of a menu line     */
+static unsigned int lines = 0;
+static unsigned int lineheight =
+    $WM_BAR_HEIGHT; /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
